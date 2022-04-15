890c0c666eb2aae767f76b85dbf083fd8380b2df
slnecnik <slnecnik@pop-os.localdomain>
2022-04-15 17:04:49 +0200

    change to pip3

--- robot-code-server/init-scripts/install_rf_and_extensions.sh
+++ robot-code-server/init-scripts/install_rf_and_extensions.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "**** installing dependencies for RF ****"
+echo "**** installing dependencies and RF ****"
 apt update
 apt install -y python3.9
 apt install -y python3.9-distutils
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3.9 get-pip.py
-python3.9 -m pip3 install robotframework
+python3.9 -m pip install robotframework
 
 
 echo "**** installing extensions ****"
